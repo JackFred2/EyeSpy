674f791774354463a5b72644469066cd3e3b0579
Jack Pearce <jack@jackf.red>
2023-12-08 21:48:01 +0000

    [feat] notification toast for the feature when the mod is installed client-side

--- src/client/java/red/jackf/eyespy/EyeSpyClientNetworking.java
+++ src/client/java/red/jackf/eyespy/EyeSpyClientNetworking.java
@@ -3,11 +3,13 @@ package red.jackf.eyespy;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import red.jackf.eyespy.networking.packets.C2SHasClientModInstalled;
+import red.jackf.eyespy.networking.packets.S2CSettings;
 
 public class EyeSpyClientNetworking {
     public static void setup() {
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             if (ClientPlayNetworking.canSend(C2SHasClientModInstalled.TYPE)) {
+                ClientPlayNetworking.registerReceiver(S2CSettings.TYPE, ((packet, player, responseSender) -> EyeSpyClient.lastSettings = packet));
                 sender.sendPacket(new C2SHasClientModInstalled());
             }
         });
