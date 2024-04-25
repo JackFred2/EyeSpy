5e9b04e0ad0300ece125bb7588348de72a039ab9
Jack Pearce <jack@jackf.red>
2024-04-25 18:11:15 +0100

    [internal] 1.20.5 update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -171,7 +171,7 @@ tasks.withType<ProcessResources>().configureEach {
 }
 
 tasks.withType<JavaCompile>().configureEach {
-	options.release.set(17)
+	options.release.set(21)
 }
 
 tasks.named<Jar>("sourcesJar") {
